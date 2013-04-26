acf25426dbfc84e351b99b9564c62f531b6dd6ae
Rob Rodi <robert.rodi@gmail.com>
2013-04-26 07:28:10 -0700

    filters

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,3 +1,7 @@
+// scope is dependency injection!
 var myController = function($scope){
-	$scope.items = [{name : "rob" }, {name : "mike"}];
+	$scope.beers = [
+						{name : "90 Minute IPA", brewer: "dogfish head", price: 3.25, brewedOn: "2013-03-15"}, 
+						{name : "Manny's Pale Ale", brewer: "georgetown", price: 1.50, brewedOn: "2013-04-12"}, 
+					];
 }
