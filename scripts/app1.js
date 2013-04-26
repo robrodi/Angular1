e92b5e6f09f7f20c92417f91755ce1f89f7bed84
Rob Rodi <robert.rodi@gmail.com>
2013-04-26 10:01:50 -0700

    broken 1

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,22 +1,18 @@
-var ngMetadata = angular.module("ngMetadata", []);
+var ngMetadata = angular.module("ngMetadata", ['ngResource']);
 
-ngMetadata.factory("Metadata", function($http){
+ngMetadata.factory("Metadata", function($resource){
 	return {
 		data: function(){  
 			return $http.get('metadata.json'); 
-		}
+		},
+		data2: $resource('metadata.json')
 	};
 });
 
 ngMetadata.controller("ListCtrl", function($scope, Metadata){
-	var metadataResult = Metadata.data();
+	var metadataResult = Metadata.data2.query({}, isArray=false);
+	$scope.metadataTypes = metadataResult.toNamedArray();
 
-	metadataResult.success(function(data){
-		$scope.metadataTypes = data.toNamedArray();
-
-		$scope.selectedType = $scope.metadataTypes[0];
-	});
-	
 	$scope.addItem = function() {
 		if (!$scope.name || $scope.name == '' || !$scope.price || $scope.price == '') return;
 		$scope.items.push({ name: $scope.name, price: $scope.price, brewedOn: new Date()});
@@ -24,5 +20,9 @@ ngMetadata.controller("ListCtrl", function($scope, Metadata){
 	$scope.removeItem = function(item){
 		if (confirm("remove this item?"))
 			$scope.items.splice($scope.items.indexOf(item), 1);
+	}
+	$scope.break = function(){
+		$scope.metadataTypes;
+		$scope.selectedType;
 	}
 });
