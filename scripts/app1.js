11831ce9364c248ed7217f15977746e4a08f039e
Rob Rodi <robert.rodi@gmail.com>
2013-04-26 09:28:59 -0700

    cleanup

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,14 +1,5 @@
 var ngMetadata = angular.module("ngMetadata", []);
 
-Object.prototype.toNamedArray = function() { 
-	var result = new Array();
-	for (var key in this)
-		result.push({ name: key, data:this[key]});
-	return result; 
-}
-
-
-// scope is dependency injection!
 ngMetadata.controller("ListCtrl", function($scope, $http){
 	var metadataResult = $http.get('metadata.json');
 
@@ -18,11 +9,6 @@ ngMetadata.controller("ListCtrl", function($scope, $http){
 		$scope.selectedType = $scope.metadataTypes[0];
 	});
 	
-	$scope.pluralizer = {
-		0 : "No Items!",
-		1 : "Only 1 item left!",
-		other : "{} items"
-	};
 	$scope.addItem = function() {
 		if (!$scope.name || $scope.name == '' || !$scope.price || $scope.price == '') return;
 		$scope.items.push({ name: $scope.name, price: $scope.price, brewedOn: new Date()});
