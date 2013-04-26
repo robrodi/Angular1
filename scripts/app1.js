585e00209c41603cc2bfb5aa641364a91c488412
Rob Rodi <robert.rodi@gmail.com>
2013-04-26 07:31:48 -0700

    pluralizer

--- scripts/app1.js
+++ scripts/app1.js
@@ -4,4 +4,9 @@ var myController = function($scope){
 						{name : "90 Minute IPA", brewer: "dogfish head", price: 3.25, brewedOn: "2013-03-15"}, 
 						{name : "Manny's Pale Ale", brewer: "georgetown", price: 1.50, brewedOn: "2013-04-12"}, 
 					];
+	$scope.pluralizer = {
+		0 : "No Beers!",
+		1 : "Only 1 beer left!",
+		other : "{} beers in fridge"
+	};
 }
