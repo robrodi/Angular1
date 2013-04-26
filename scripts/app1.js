f3ed1375c33972cfd35b03e52b002e915a4a84b5
Rob Rodi <robert.rodi@gmail.com>
2013-04-26 08:07:07 -0700

    beer to items

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,20 +1,20 @@
 // scope is dependency injection!
 var myController = function($scope){
-	$scope.beers = [
+	$scope.items = [
 						{name : "90 Minute IPA", brewer: "dogfish head", price: 3.25, brewedOn: "2013-03-15"}, 
 						{name : "Manny's Pale Ale", brewer: "georgetown", price: 1.50, brewedOn: "2013-04-12"}, 
 					];
 	$scope.pluralizer = {
-		0 : "No Beers!",
-		1 : "Only 1 beer left!",
-		other : "{} beers in fridge"
+		0 : "No Items!",
+		1 : "Only 1 item left!",
+		other : "{} items in fridge"
 	};
-	$scope.addBeer = function() {
+	$scope.addItem = function() {
 		if (!$scope.name || $scope.name == '' || !$scope.price || $scope.price == '') return;
-		$scope.beers.push({ name: $scope.name, price: $scope.price, brewedOn: new Date()});
+		$scope.items.push({ name: $scope.name, price: $scope.price, brewedOn: new Date()});
 	}
-	$scope.removeBeer = function(beer){
-		if (confirm("remove this beer?"))
-			$scope.beers.splice($scope.beers.indexOf(beer), 1);
+	$scope.removeItem = function(item){
+		if (confirm("remove this item?"))
+			$scope.items.splice($scope.items.indexOf(item), 1);
 	}
 }
