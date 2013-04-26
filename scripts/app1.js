d265cb684e9e8114f50f38bd274181215ac1b8ed
Rob Rodi <robert.rodi@gmail.com>
2013-04-26 09:33:19 -0700

    factory thing

--- scripts/app1.js
+++ scripts/app1.js
@@ -1,7 +1,15 @@
 var ngMetadata = angular.module("ngMetadata", []);
 
-ngMetadata.controller("ListCtrl", function($scope, $http){
-	var metadataResult = $http.get('metadata.json');
+ngMetadata.factory("Metadata", function($http){
+	return {
+		data: function(){  
+			return $http.get('metadata.json'); 
+		}
+	};
+});
+
+ngMetadata.controller("ListCtrl", function($scope, Metadata){
+	var metadataResult = Metadata.data();
 
 	metadataResult.success(function(data){
 		$scope.metadataTypes = data.toNamedArray();
